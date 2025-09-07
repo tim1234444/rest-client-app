8903d93742086db85e4464c75d547d79b89f14bd
1 <tim1234444>
2025-09-07 17:58:03 +0300

    feat: Created a request recovery button

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { useRouter } from 'next/navigation'
+import RowButton from '../RowButton/RowButton';
+
 interface IProps {
   request_duration: number;
   response_status_code: number;
@@ -15,41 +15,37 @@ interface IProps {
 }
 
 export default function Row({ index, value, id }: { index: number; value: IProps; id: number }) {
-  const router = useRouter();
   return (
-    <tr
-      key={index}
-      data-id={id}
-      onClick={() => {
-        const requstMethod = value.request_method;
-        const  requestUrl = window.btoa(value.endpoint_url);
-        const  requestHeader = value.request_headers ? `?${value.request_headers}` : '';
-        const  requestBody = value.request_body ? `/${window.btoa(value.request_body)}` : '';
-        router.push('/protected/client/' + requstMethod +'/' + requestUrl + requestBody + requestHeader);
-      }}
-      className="hover:bg-muted/30 cursor-pointer"
-    >
-      {Object.entries(value).map(([key, val], i) => {
-        let displayValue = val;
+    <>
+      <tr key={index} data-id={id} className="hover:bg-muted/30 cursor-pointer">
+        {Object.entries(value).map(([key, val], i) => {
+          let displayValue = val;
+          if (key === 'request_headers' || key === 'request_body' || key === 'id') {
+            return null;
+          }
+          if (key === 'request_timestamp' && val) {
+            displayValue = new Date(val as string).toLocaleString('en-GB', {
+              dateStyle: 'short',
+              timeStyle: 'medium',
+            });
+          }
 
-        if (key === 'request_timestamp' && val) {
-          displayValue = new Date(val as string).toLocaleString('en-GB', {
-            dateStyle: 'short',
-            timeStyle: 'medium',
-          });
-        }
-
-        return (
-          <td
-            key={i}
-            className={`px-4 py-2 text-sm ${
-              i === 0 ? 'font-medium text-foreground' : 'text-muted-foreground'
-            }`}
-          >
-            {displayValue}
-          </td>
-        );
-      })}
-    </tr>
+          return (
+            <td
+              key={i}
+              className={`px-4 py-2 text-sm ${
+                i === 0 ? 'font-medium text-foreground' : 'text-muted-foreground'
+              }`}
+            >
+              {displayValue}
+            </td>
+          );
+        })}
+        <td className="text-muted-foreground px-4 py-2 text-sm">
+          {' '}
+          <RowButton value={value}></RowButton>
+        </td>
+      </tr>
+    </>
   );
 }
---
+++ src/components/RowButton/RowButton.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+interface IProps {
+    request_duration: number;
+    response_status_code: number;
+    request_timestamp: string;
+    request_method: string;
+    request_size: number;
+    response_size: number;
+    error_details: string;
+    endpoint_url: string;
+    id: number;
+    request_headers: string;
+    request_body: string;
+  }
+export default function RowButton({value}: {value: IProps}) {
+  const router = useRouter();
+  return (
+    <button
+    onClick={() => {
+      const requestMethod = value.request_method;
+      const requestUrl = window.btoa(value.endpoint_url);
+      const requestHeader = value.request_headers ? `?${value.request_headers}` : '';
+      const requestBody = value.request_body ? `/${window.btoa(value.request_body)}` : '';
+      router.push(
+        '/protected/client/' + requestMethod + '/' + requestUrl + requestBody + requestHeader,
+      );
+    }}
+    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200"
+  >
+    Restore request
+  </button>
+  
+  );
+}
