c285c1398f27e85b02a22eb8b583fcb06d644e8a
AlexDrags <ksander-88@yandex.ru>
2025-09-11 20:35:23 +0300

    feat:add react compier techology

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
 };
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
