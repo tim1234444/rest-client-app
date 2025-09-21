148e5f50fc00c10018d7de43c5da940c02fad81f
AlexDrags <ksander-88@yandex.ru>
2025-09-21 18:39:36 +0300

    feat:add versel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,13 +5,13 @@ import { NextIntlClientProvider } from 'next-intl';
 import './globals.css';
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+  ? `https://${process.env.VERCEL_URL}/en`
   : 'http://localhost:3000';
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Rest Client',
+  description: 'RSSchool project to build apps with Next.js and Supabase',
 };
 
 const geistSans = Geist({
