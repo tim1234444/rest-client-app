1d6631218b3bcbfb7376bd458c3aa4a238391b7e
AlexDrags <47152229+AlexDrags@users.noreply.github.com>
2025-09-21 19:53:01 +0300

    fix: Update sendRequest.ts

--- src/utils/sendRequest.ts
+++ src/utils/sendRequest.ts
@@ -4,11 +4,13 @@ export async function sendRequest(data: {
   headers: Record<string, string>;
   body: string;
 }) {
-  const res = await fetch('/API/writeRow', {
+  const res = await fetch(`/API/writeRow`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
-  console.log('CONSOLE', res);
-  return res.json().then((info) => ({ status: res.status, data: info }));
+  console.log('CONSOLE',res);
+  const json = await res.json();
+  console.log('CONSOLE2', { status: res.status, data: json });
+  return { status: res.status, data: json };
 }
